2affd68ed87c1549ce1f8dde58f65c730dfc9dfd
Arashika <amaterr199706@gmail.com>
2018-04-03 20:20:09 +0300

    Показ и переключение секций

---
+++ js/main.js
@@ -0,0 +1,53 @@
+let consts = {
+  RIGHT_KEYCODE: 39,
+  LEFT_KEYCODE: 37
+};
+const arr = [];
+const sectionMain = document.querySelector(`section.main`);
+const template = document.querySelector(`#templates`);
+const mainWelcome = template.content.querySelector(`.main--welcome`).cloneNode(true);
+const mainLevelGenre = template.content.querySelector(`.main--level-genre`).cloneNode(true);
+const mainLevelArtist = template.content.querySelector(`.main--level-artist`).cloneNode(true);
+const mainResult = template.content.querySelector(`.main--result`).cloneNode(true);
+arr.push(mainWelcome);
+arr.push(mainLevelGenre);
+arr.push(mainLevelArtist);
+arr.push(mainResult);
+
+const showSection = function (i) {
+  sectionMain.appendChild(arr[i]);
+};
+
+const removeSection = function (i) {
+  sectionMain.removeChild(arr[i]);
+};
+showSection(0);
+
+let i = 0;
+
+document.addEventListener(`keydown`, function (evt) {
+  if (evt.altKey === true && evt.keyCode === consts.RIGHT_KEYCODE) {
+    if (i === arr.length - 1) {
+      removeSection(i);
+      i = 0;
+      showSection(0);
+    } else {
+      removeSection(i);
+      i++;
+      showSection(i);
+    }
+  }
+  if (evt.altKey === true && evt.keyCode === consts.LEFT_KEYCODE) {
+    if (i === arr.length - arr.length) {
+      removeSection(i);
+      i = 3;
+      showSection(3);
+    } else {
+      removeSection(i);
+      i--;
+      showSection(i);
+    }
+  }
+});
+
+
