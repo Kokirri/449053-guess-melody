d5c5f3f349f918d67974967c2c7e75302bdb0c2f
Arashika <amaterr199706@gmail.com>
2018-04-10 19:59:05 +0300

    Разобрали секции на отдельные модули(2)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 import moduleWelcome from './module-welcome';
 import moduleGenre from "./module-genre";
 import moduleArtist from "./module-artist";
-import moduleResult from "./result/module-result";
+import moduleResult from "result/module-result";
 
-
+const arr = [];
+const mainWelcome = moduleWelcome;
+const mainLevelGenre = moduleGenre;
+const mainLevelArtist = moduleArtist;
+const mainResult = moduleResult;
+arr.push(mainWelcome);
+arr.push(mainLevelGenre);
+arr.push(mainLevelArtist);
+arr.push(mainResult);
