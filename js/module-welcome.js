cf3f450726b896a73419afdb62055096962561b5
Arashika <amaterr199706@gmail.com>
2018-04-10 19:53:29 +0300

    Разобрали секции на отдельные модули

---
+++ js/module-welcome.js
@@ -0,0 +1,23 @@
+import {getElement} from './get-element';
+import {showSection, removeSection} from './show-section.js';
+import moduleArtist from "./module-artist";
+
+const moduleWelcome = getElement(`<section class="main main--welcome">
+    <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
+    <button class="main-play">Начать игру</button>
+    <h2 class="title main-title">Правила игры</h2>
+    <p class="text main-text">
+      Правила просты&nbsp;— за&nbsp;5 минут ответить на все вопросы.<br>
+      Ошибиться можно 3 раза.<br>
+      Удачи!
+    </p>
+  </section>`);
+
+showSection(moduleWelcome);
+const playButton = moduleWelcome.querySelector(`.main-play`);
+playButton.addEventListener(`click`, function () {
+  removeSection(moduleWelcome);
+  showSection(moduleArtist);
+});
+
+export default moduleWelcome;
